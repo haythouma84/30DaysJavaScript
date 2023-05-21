697ac34c8b877bd11485dd01892730ca043ddf83
Shoukry <hshoukry@mgc.mentorg.com>
2023-05-21 23:03:19 +0300

    Day1 Files

---
+++ Day1/datatypes.js
@@ -0,0 +1,28 @@
+console.log(typeof(str_var));
+console.log(typeof(undef_var));
+let x, y, z, h;
+var x1 = 1 ;
+var x2 = 2 ;
+var x3 = 3 ;
+var x4 = 4 ;
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+var fname = "Haytham";
+var lname = "Saafan";
+var mstatus = "Married";
+var country = "Egypt";
+var age = 39 ;
+console.log(fname);
+console.log(lname);
+console.log(mstatus);
+console.log(country);
+console.log(age);
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+console.log(fname,lname,mstatus,country,age);
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console
+let myAge = 25 ;
+let yourAge = 30 ;
+console.log("I am " + myAge + " years old.");
+console.log("You are " + yourAge + " years old.")
---
+++ Day1/variables.js
@@ -0,0 +1,4 @@
+var str_var = "Hello World!";
+var bool_var = true;
+let undef_var = undefined;
+let null_var = null;
